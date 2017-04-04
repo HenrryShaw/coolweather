994c34011377332222c66bc283420261fc799a07
HenrryShaw <1033692034@qq.com>
2017-04-04 23:03:32 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,22 @@
+package com.coolweather.android.util;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by Mr.1 on 2017/4/4.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(address)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+}
